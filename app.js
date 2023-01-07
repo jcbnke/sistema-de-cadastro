20ae22efd299eaeebed204fe4f522d9cb6e433df
jcbnke <86270916+jcbnke@users.noreply.github.com>
2023-01-07 09:55:52 -0300

    Para o merge teste

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Fazer o merge da desenvolvimento para a Master")
+console.log("Fazer o merge da desenvolvimento para a Master teste")
